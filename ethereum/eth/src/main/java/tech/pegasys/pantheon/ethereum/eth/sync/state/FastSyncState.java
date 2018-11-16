9adf9756efd6ef3c39ede27c4903f1f55649ec6d
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2018-11-16 19:39:50 +0300

    Add fields to FastSyncState

--- ethereum/eth/src/main/java/tech/pegasys/pantheon/ethereum/eth/sync/state/FastSyncState.java
+++ ethereum/eth/src/main/java/tech/pegasys/pantheon/ethereum/eth/sync/state/FastSyncState.java
@@ -12,28 +12,45 @@
  */
 package tech.pegasys.pantheon.ethereum.eth.sync.state;
 
+import tech.pegasys.pantheon.ethereum.core.Hash;
 import tech.pegasys.pantheon.ethereum.eth.sync.SynchronizerConfiguration;
 
 public final class FastSyncState {
-  private long fastSyncTargetBlockNumber = -1;
+
+  public enum State {
+    Initial,
+    HeadersDownload,
+    StateDownload,
+    BlockBodiesDownload,
+    ReceiptsDownload,
+    Complete
+  }
 
   private final SynchronizerConfiguration config;
+
+  private State state = State.Initial;
+
+  private Hash lastHeaderHash;
+  private Hash pivotBlockHash;
+  private long stateNodesComplete;
+  private Hash lastBlockBodyHash;
+  private Hash lastReceiptsBlockHash;
 
   public FastSyncState(final SynchronizerConfiguration config) {
     this.config = config;
   }
 
-  /**
-   * Registers the chain height that we're trying to sync to.
-   *
-   * @param blockNumber the height of the chain we are syncing to.
-   */
-  public void setFastSyncChainTarget(final long blockNumber) {
-    fastSyncTargetBlockNumber = blockNumber;
-  }
-
-  /** @return the block number at which we switch from fast sync to full sync */
-  public long pivot() {
-    return Math.max(fastSyncTargetBlockNumber - config.fastSyncPivotDistance(), 0);
-  }
+//  /**
+//   * Registers the chain height that we're trying to sync to.
+//   *
+//   * @param blockNumber the height of the chain we are syncing to.
+//   */
+//  public void setFastSyncChainTarget(final long blockNumber) {
+//    pivotBlockNumber = blockNumber;
+//  }
+//
+//  /** @return the block number at which we switch from fast sync to full sync */
+//  public long pivot() {
+//    return Math.max(pivotBlockNumber - config.fastSyncPivotDistance(), 0);
+//  }
 }
